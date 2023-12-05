ab1de111d7321b215c60f96d6e973f50b0292367
Eli-Satter-card-decks <151986436+Eli-Satter-card-decks@users.noreply.github.com>
2023-12-05 02:08:48 -0600

    Update chip.java

--- chip.java
+++ chip.java
@@ -11,10 +11,9 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class chip{
+public class chips{
 public static int bet=0;
 public static int chips=50;
-public static int round=1;
 static JFrame frame=new JFrame("Chip Gambles");
     static JPanel mainPanel = new JPanel();
 	static JPanel gamblingwindow=new JPanel();
@@ -24,14 +23,15 @@ static JFrame frame=new JFrame("Chip Gambles");
     static JLabel even=new JLabel("");
     static JLabel score=new JLabel("");
     static JLabel endchipcount=new JLabel("");
-	static JLabel algorithmex=new JLabel("Get a 3, 5, or a 7 to win at low stakes, and either 5 and 3, 5, or 7, OR 6 and 6 to win at high stakes.");
+    static JLabel special=new JLabel("WATCH HERE");
+	static JLabel algorithmex=new JLabel("Get a 3, 5, or a 7 to win at low stakes, and either 5 and 3, 5, or 7, OR 6 and 6 to lose at high stakes.");
 	static JTextArea setchips=new JTextArea("Call how many chips you'ii bet");
 	static JButton lowstakes=new JButton("Low Stakes");
 	static JButton highstakes=new JButton("High Stakes");
 	static JButton endgame=new JButton("End the game");
     static JLabel keepgoing=new JLabel("");
-	static JLabel chipcount=new JLabel("Round "+ round+". "+chips+" Chips.");
-public static String confusin=null;
+	static JLabel chipcount=new JLabel("100");
+public static String confusin="";
 	 public static void main(String[] args) {
 	
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,6 +44,7 @@ public static String confusin=null;
 	frame.add(mainPanel);
 	mainPanel.add(gamblingwindow);
 	mainPanel.add(finalscore);
+gamblingwindow.add(special);
 	gamblingwindow.add(odd);
 	gamblingwindow.add(even);
 	gamblingwindow.add(score);
@@ -55,10 +56,11 @@ public static String confusin=null;
 	gamblingwindow.add(gambleingcall);
 	gamblingwindow.add(endgame);
 	gamblingwindow.add(chipcount);
-	chipcount.setBounds(50, 200, 350, 50);
+	chipcount.setBounds(50, 200, 50, 50);
 	lowstakes.setBounds(50, 300, 150, 100);
 	highstakes.setBounds(50, 425, 150, 100);
 	setchips.setBounds(50, 50, 250, 50);
+special.setBounds(50, 150, 150, 50);
 	algorithmex.setBounds(50, 500, 800, 100);
 	keepgoing.setBounds(50, 700, 800, 75);
 	endgame.setBounds(50, 600, 250, 75);
@@ -77,9 +79,10 @@ public static String confusin=null;
  
 
         public void actionPerformed(ActionEvent e) {
-			 round=round+1;
+			 highstakes.setEnabled(false);
 			 bet=Integer.parseInt(setchips.getText());
 			 
+			 chips=Integer.parseInt(chipcount.getText());
 			 int vbl=5;
 			 int minimumcall=3;
 int setrange=2;
@@ -96,14 +99,17 @@ if( (rando-vbl)== setrange || (vbl - rando) ==setrange ){
 	}
 	else {chips = chips - 2*bet;}
 		String hooray=Integer.toString(chips);
-		 chipcount.setText("Round "+round+". "+hooray+" Chips.");
+		 chipcount.setText(hooray);
 		   if ((rando == 3) || (rando==7)){
-                            confusin =  ("You drew a " +rando+", you got "+(bet * 3) +" chips!");
+                 //           confusin =  ("You drew a " +rando+", you got "+(bet * 3) +" chips!");
+special.setText("√");
                     }else if (rando==5){
-                            confusin =  ("You drew a " +rando+", you got "+(bet * 6) +" chips!");
+                //            confusin =  ("You drew a " +rando+", you got "+(bet * 6) +" chips!");
+special.setText("√ √");
                         }
                             else
-                           confusin =  ("You drew a " +rando+", you lost "+(bet * 2) +" chips!");
+              //             confusin =  ("You drew a " +rando+", you lost "+(bet * 2) +" chips!");
+special.setText("██");
                         
 						algorithmex.setText(confusin);
 		}}
@@ -113,9 +119,9 @@ if( (rando-vbl)== setrange || (vbl - rando) ==setrange ){
  @Override
 
         public void actionPerformed(ActionEvent e) {
-			 round=round+1;
+			 lowstakes.setEnabled(false);
 			 bet=Integer.parseInt(setchips.getText());
-
+			 chips=Integer.parseInt(chipcount.getText());
 			 int vbl=5;
 			 int minimumcall=3;
 			 int oddpoint=0;
@@ -140,30 +146,36 @@ if (rando%n==0){
                         }
                         if (rando == (oddpoint + vbl)) {
                             System.out.println();
-                            chips = chips + 12 * bet;
+                            chips = chips + 5 * bet;
                         } else if (oddpoint == 1 && (rando - (vbl + oddpoint) == setrange || (vbl + oddpoint) - rando == setrange)) {
-                            chips = chips - bet * 3;
+                            chips = chips - bet * 7;
                         } else {
-                            chips = chips - 1 * bet;
+                            chips = chips - 4 * bet;
                         }
 		String hooray=Integer.toString(chips);
-		 chipcount.setText("Round "+round+". "+hooray+" Chips");
+		 chipcount.setText(hooray);
 		 
 		   if ((((rando== 3) || (rando == 7))&&(oddpoint==0))) {
-                        confusin = ("You drew a " +(vbl + oddpoint) +" and a " + rando+", you got "+(bet * 3)+" chips! ");
+            //            confusin = ("You drew a " +(vbl + oddpoint) +" and a " + rando+", you got "+(bet * 3)+" points! ");
+special.setText("██ ██ ██");
                     }
                     else
                     if ((rando == 5) && ((oddpoint)==0)){
-                        confusin = ("You drew a " +(vbl + oddpoint) + " and a " + rando+", you got "+(17 * bet)+" chips! ");
+          //              confusin = ("You drew a " +(vbl + oddpoint) + " and a " + rando+", you got "+(10 * bet)+" points! ");
+special.setText("██ ██ ██ ██");
                     }
                     else
-                        confusin = ("You drew a " +(vbl + oddpoint) + " and a " + rando+", you lost "+(2 * bet)+" chips! ");
+        //                confusin = ("You drew a " +(vbl + oddpoint) + " and a " + rando+", you lost "+(2 * bet)+" points! ");
+special.setText("√ √");
                     if ((rando==6) && ((oddpoint) == 1)){
-                        confusin = ("You drew a " +(vbl + oddpoint) + " and a " + rando+", you got "+(11*bet)+" chips! ");
+      //                  confusin = ("You drew a " +(vbl + oddpoint) + " and a " + rando+", you got "+(11*bet)+" points! ");
+special.setText("██ ██");
                     } else if (((oddpoint) == 1) && ((rando==4) || (rando == 8))){
-                        confusin = ("You drew a " +(vbl + oddpoint) + " and a " + rando+", you lost "+(4 * bet)+" chips! ");
+    //                    confusin = ("You drew a " +(vbl + oddpoint) + " and a " + rando+", you lost "+(4 * bet)+" points! ");
+special.setText("√ √ √ √");
                     }else if ((oddpoint==1)){
-                        confusin = ("You drew a "+(vbl + oddpoint) + " and a " + rando+", you lost "+(2 * bet)+" chips! ");
+  //                      confusin = ("You drew a "+(vbl + oddpoint) + " and a " + rando+", you lost "+(2 * bet)+" points! ");
+special.setText("√ √");
                     }
 
 
@@ -175,11 +187,10 @@ if (rando%n==0){
 public static class restartee implements ActionListener{
 	@Override
        public void actionPerformed(ActionEvent e){
-		   chips=50;
-		   round=1;
+lowstakes.setEnabled(true);
+highstakes.setEnabled(true);
 		   keepgoing.setText("you ended with "+chips+" chips your last time at the table");
-		chipcount.setText("Round "+ round+". "+chips+" Chips.");
-		
+		chipcount.setText("100");
 		}
 }
 }
